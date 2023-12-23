ad6aceae2f9fade4d0f809c424fad7bff6d2e186
hvmanker <himankverma121@gmail.com>
2023-12-23 22:32:51 +0530

    vercel push

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const taskRouter = require('./routes/taskroutes');
-const connectDB = require('./config/dbconnect')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const taskRouter = require("./routes/taskroutes");
+const connectDB = require("./config/dbconnect");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,11 +11,12 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.use("/", (req, res) => {
+  res.send("api working fine");
+});
 
-
-
-app.use('/tasks', taskRouter);
+app.use("/tasks", taskRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
